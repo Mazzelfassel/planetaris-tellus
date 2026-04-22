c05ecdf0a6c9f27cd5f67d7bcda8ec51aad6a058
Syen-ce <angellecce2003@gmail.com>
2026-04-22 19:53:23 +0200

    1.1.2

--- prototypes/preservation-item.lua
+++ prototypes/preservation-item.lua
@@ -5,6 +5,32 @@ local space_age_item_sounds = require("__space-age__.prototypes.item_sounds")
 local item_tints = require("__base__.prototypes.item-tints")
 local item_effects = require("__space-age__.prototypes.item-effects")
 
+local function build_preservation_icons(preserved_item, spoilable_item)
+  if spoilable_item.icon then
+    preserved_item.icons = {
+      {icon="__planetaris-tellus__/graphics/icons/preservation-item.png", draw_background=false, scale=0.5},
+      {icon=spoilable_item.icon, scale=0.3},
+      {icon="__planetaris-tellus__/graphics/icons/preservation-item-top.png", scale=0.5}
+    }
+    preserved_item.icon = nil
+  end
+  if spoilable_item.icons then
+    local new_icons = {
+      {icon="__planetaris-tellus__/graphics/icons/preservation-item.png", draw_background=false, scale=0.5}
+    }
+    for _, icons_table in pairs(spoilable_item.icons) do
+      local copy = {}
+      for k, v in pairs(icons_table) do
+        copy[k] = v
+      end
+      copy.scale = 0.3
+      table.insert(new_icons, copy)
+    end
+    table.insert(new_icons, {icon="__planetaris-tellus__/graphics/icons/preservation-item-top.png", scale=0.5})
+    preserved_item.icons = new_icons
+  end
+end
+
 add_preservation_blacklist = {
   "planetaris-unstable-shard",
   "planetaris-unstable-gem",
@@ -65,11 +91,7 @@ for _, spoilable_item in pairs(preservation_whitelist) do
       preserved_item.name = spoilable_item.name.."-preserved"
       preserved_item.localised_name = {"", spoilable_item.localised_name or {"item-name."..spoilable_item.name}, " ", {"preservation-name.preserved"}}
       preserved_item.localised_description = {"preservation-description.preserved"}
-      preserved_item.icons = {
-        {icon="__planetaris-tellus__/graphics/icons/preservation-item.png", draw_background=false, scale=0.5},
-        {icon=spoilable_item.icon or spoilable_item.icons, scale=0.3},
-        {icon="__planetaris-tellus__/graphics/icons/preservation-item-top.png", scale=0.5}
-      }
+      build_preservation_icons(preserved_item, spoilable_item)
       if preserved_item.plant_result then preserved_item.plant_result = nil end
       if preserved_item.place_result then preserved_item.place_result = nil end
       if preserved_item.place_as_tile then preserved_item.place_as_tile = nil end
@@ -77,6 +99,10 @@ for _, spoilable_item in pairs(preservation_whitelist) do
       preserved_item.spoil_ticks = preserved_item.spoil_ticks * 1.5
       preserved_item.fuel_category = nil
       preserved_item.fuel_value = nil
+      preserved_item.fuel_acceleration_multiplier = nil
+      preserved_item.fuel_top_speed_multiplier = nil
+      preserved_item.fuel_emissions_multiplier = nil
+      preserved_item.fuel_glow_color = nil
       preserved_item.hidden_in_factoriopedia = settings.startup["hide-preserved-items"].value
       if preserved_item.weight then preserved_item.weight = 100 * kg end
       preserved_item.subgroup = "tellus-preserved"
@@ -119,16 +145,7 @@ for _, spoilable_item in pairs(preservation_whitelist) do
           type = "recipe",
           name = spoilable_item.name.. "-depreservation",
           localised_name = {"", spoilable_item.localised_name or {"item-name."..spoilable_item.name}, " ", {"preservation-name.depreservation"}},
-          icons = {
-            {icon=spoilable_item.icon or spoilable_item.icons},
-            {
-            icon = "__planetaris-tellus__/graphics/icons/constant-preservation.png",
-            icon_size = 128,
-            scale = 0.18,
-            shift = {10, 10},
-            floating = true
-            },
-          },
+          icons = spoilable_item.icons or {{icon=spoilable_item.icon}},
           subgroup = "tellus-depreserved-recipe",
           order = "a",
           category = "preserving",
@@ -156,5 +173,13 @@ for _, spoilable_item in pairs(preservation_whitelist) do
           show_amount_in_title = false
         },
       })
+      -- Add icon
+        table.insert(data.raw.recipe[spoilable_item.name.. "-depreservation"].icons, {
+            icon = "__planetaris-tellus__/graphics/icons/constant-preservation.png",
+            icon_size = 128,
+            scale = 0.18,
+            shift = {10, 10},
+            floating = true
+            })
     end
 end
